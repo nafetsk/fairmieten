1460cfcc5d24912713087ad87ac3ad6196085d2b
grischan <grischan@gmail.com>
2024-09-29 17:14:56 +0200

    umstellung auf uuid

--- fairmieten/models.py
+++ fairmieten/models.py
@@ -1,15 +1,16 @@
+import uuid
 from django.db import models
 
 
 class Diskrimminierungsart(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
 class Diskriminierung(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     name = models.CharField(max_length=100)
     typ = models.ForeignKey(Diskrimminierungsart, on_delete=models.CASCADE)
 
@@ -17,22 +18,22 @@ class Diskriminierung(models.Model):
         return self.name
 
 class Loesungsansaetze(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     name = models.CharField(max_length=100)
 
 class Ergebnis(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     name = models.CharField(max_length=100)
 
 # Betroffene Person
 class Person(models.Model): 
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     alter_item = models.CharField(max_length=100) # String weil Kohorten
     anzahl_kinder = models.IntegerField(null=True, blank=True)
     gender_item = models.CharField(max_length=100)
 
 class Vorgang(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     fallnummer = models.IntegerField(null=True, blank=True)
     vorgangstyp_item = models.CharField(max_length=100, null=True, blank=True) # allgemeine Beratung, Meldung, Fallbetreuung
     datum_kontakaufnahme = models.DateField(null=True, blank=True)
@@ -54,13 +55,13 @@ class Vorgang(models.Model):
 
 # Verursacher
 class Verursacher(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     unternehmenstyp_item = models.CharField(max_length=100) # (städtisch, land, privat usw.)
     personentyp_item = models.CharField(max_length=100) # (Hausmeister, Hausverwaltung)
     vorgang = models.ForeignKey(Vorgang, on_delete=models.CASCADE)
 
 class Intervention(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     vorgang_id = models.ForeignKey(Vorgang, on_delete=models.CASCADE)
     datum = models.DateField()
     form_item = models.CharField(max_length=100)
@@ -68,13 +69,13 @@ class Intervention(models.Model):
 
 
 class Item(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     key = models.CharField(max_length=100)  # TODO: bessere Datentyp?
     value = models.CharField(max_length=100)
 
 
 class Charts(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.UUIDField( primary_key = True, default = uuid.uuid4, editable = False) 
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=100)
 
