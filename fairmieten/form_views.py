21112a61754d775b3b1ffbdcb441b581a4f8433a
grischan <grischan@gmail.com>
2024-09-30 17:35:26 +0200

    umstellung form_view auf create_xxx form

--- fairmieten/form_views.py
+++ fairmieten/form_views.py
@@ -1,7 +1,8 @@
+import uuid
 from datetime import datetime
 from django.shortcuts import render
 from .forms import PersonForm, VorgangForm
-from .models import Vorgang
+from .models import Vorgang, Person
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
@@ -11,52 +12,56 @@ def vorgang_liste(request):
 
 form_liste = [
     {'key':'vorgang', 'label':'Allgemein', 'form': VorgangForm},
-    {'key':'person', 'label':'Person', 'form': PersonForm},
-    {'key':'diskriminierung', 'label':'Falltypologie', 'form': PersonForm}
+    {'key':'person', 'label':'Person', 'form': PersonForm}
 ]
 
 
 def vorgang_erstellen(request):
-    return render(request, 'add_vorgang.html', {'form_liste': form_liste})
+	vorgang_id = uuid.uuid4()
+	return render(request, 'add_vorgang.html', {'form_liste': form_liste, 'vorgang_id': vorgang_id})
 
 def get_Instance(request, model:models.Model, id_name:str = 'id'):
 	id = request.GET.get(id_name, None)
-	if (id is None or id == 'None'):
-		instance = model()
-	else:
-		try:
-			instance = model.objects.get(id=id)
-		except ObjectDoesNotExist:
-			instance = model()  
+	if (id is None or id == 'None'): id = uuid.uuid4()
+	instance, created = model.objects.get_or_create(id=id)
 	return instance
 
+def get_Foreign_Instance(request, model:models.Model, id_name:str = 'id'):
+	vorgang_id = request.GET.get("vorgang_id", None)
+	if (vorgang_id is None or vorgang_id == 'None'):
+		raise Exception("Vorgang ID ist erforderlich")
+	id = request.GET.get(id_name, None)
+	if (id is None or id == 'None'): id = uuid.uuid4()
+	instance, created = model.objects.get_or_create(id=id, vorgang_id=vorgang_id)
+	return instance
+
+
 def save_form(request, form_nr:int):
-	VF = form_liste[int(form_nr)]['form']
+	tuple = form_liste[int(form_nr)]
+	VF = tuple['form']
 	instance = get_Instance(request, VF.Meta.model)
-	form = VF(request.POST or None, instance=instance)
+	form = VF(request.POST, instance=instance)
 	if form.is_valid():
 		form.save()
-	uhrzeit = datetime.now().strftime("%H:%M:%S") #TODO: wieder rausnehmen
-	return render(request, 'inner_form.html', {'form': form, 'uhrzeit': uhrzeit, 'form_nr': form_nr})
+	return render(request, 'inner_form.html', {'form': form, 'form_nr': form_nr})
 
 
-def vorgang_form(request):
+def create_vorgang(request):
+	vorgang = get_Instance(request, Vorgang)
 	if request.method == 'POST':
-		form = VorgangForm(request.POST)
+		form = VorgangForm(request.POST, instance=vorgang)
 		if form.is_valid():
 			form.save()
 	else:
 		form = VorgangForm()
-	return render(request, 'inner_form.html', {'form': form})
+	return render(request, 'inner_form.html', {'form': form, 'item_key': 'vorgang', 'vorgang_id': form.instance.id})
 
-def person_form(request):
-	if request.method == 'POST':
-		form = PersonForm(request.POST)
-		if form.is_valid():
-			form.save()
-	else:
-		form = PersonForm()
-	return render(request, 'inner_form.html', {'form': form})
+def create_person(request):
+	person = get_Foreign_Instance(request, Person)
+	form = PersonForm(request.POST or None, instance=person)
+	if form.is_valid():
+		form.save()
+	return render(request, 'inner_form.html', {'form': form, 'item_key': 'person', 'vorgang_id': person.vorgang_id})
 
 def diskriminierung_form(request):
 	if request.method == 'POST':
@@ -67,7 +72,6 @@ def diskriminierung_form(request):
 		form = PersonForm()
 	return render(request, 'inner_form.html', {'form': form})
 
-    
 
 def next_form(request):
     current_index = request.GET.get('current', 0)
