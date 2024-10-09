5bcab1df1ba30bb3bee314b782db3f34dd7b40e1
stefan <stefan_kl@posteo.de>
2024-10-09 15:18:37 +0200

    lauter Kleinigkeiten

--- aggregation/models.py
+++ aggregation/models.py
@@ -7,6 +7,7 @@ class Charts(models.Model):
     type = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    x_label = models.CharField(max_length=100, null=True, blank=True)
     variable = models.CharField(max_length=100, null=True, blank=True)
     model = models.CharField(max_length=100, null=True, blank=True)
 
