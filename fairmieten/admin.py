1efeebbf162cc774e55926cfb447ec459b1f3e54
grischan <grischan@gmail.com>
2024-10-16 12:05:59 +0200

    unfold admin

--- fairmieten/admin.py
+++ fairmieten/admin.py
@@ -1,26 +1,40 @@
+import unfold
 from django.contrib import admin
 from .models import (
     Diskrimminierungsart,
     Diskriminierung,
+    FormValues,
+    FormLabels,
     Loesungsansaetze,
     Ergebnis,
     Vorgang,
     Intervention,
-    Item,
     Person,
     Verursacher,
 )
 from aggregation.models import Charts
 
+class FormLabelsAdmin(unfold.admin.ModelAdmin):
+    list_display = ('model', 'field', 'label')
+    search_fields = ('model', 'field', 'label')  # Ermöglicht die Suche nach 'field' und 'label'
+    ordering = ('model', 'field')
+
+class FormValuesAdmin(admin.ModelAdmin):
+    list_display = ('model', 'field', 'key', 'value')
+    search_fields = ('model', 'field', 'value')  # Ermöglicht die Suche nach 'field' und 'label'
+    ordering = ('model', 'field')
 
 # Register your models here.
-admin.site.register(Diskrimminierungsart)
-admin.site.register(Diskriminierung)
+admin.site.register(Diskrimminierungsart, unfold.admin.ModelAdmin)
+admin.site.register(Diskriminierung, unfold.admin.ModelAdmin)
 admin.site.register(Loesungsansaetze)
 admin.site.register(Ergebnis)
 admin.site.register(Vorgang)
 admin.site.register(Intervention)
-admin.site.register(Item)
+admin.site.register(FormValues, FormValuesAdmin)
+admin.site.register(FormLabels,FormLabelsAdmin)
 admin.site.register(Charts)
 admin.site.register(Person)
 admin.site.register(Verursacher)
+
+
