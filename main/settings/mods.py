9c90b9c8eca2dc8db7bb28433f3bdef98102b944
grischan <grischan@gmail.com>
2024-10-16 12:05:59 +0200

    unfold admin

--- main/settings/mods.py
+++ main/settings/mods.py
@@ -1,9 +1,10 @@
 import os
 import environ
+from django.templatetags.static import static
 from .main import INSTALLED_APPS, BASE_DIR, MIDDLEWARE
 
 
-INSTALLED_APPS += ["environ", "fairmieten", "main.hello", "aggregation"]
+INSTALLED_APPS = [ "unfold"] + INSTALLED_APPS + [ "environ", "fairmieten", "main.hello", "aggregation"]
 
 MIDDLEWARE += ['django_htmx.middleware.HtmxMiddleware']
 
@@ -34,4 +35,35 @@ DATABASES = {
     # read os.environ['DATABASE_URL'] and raises
     # ImproperlyConfigured exception if not found
     "default": env.db_url("DATABASE_URL", default="sqlite:////tmp/my-tmp-sqlite.db")
+}
+
+UNFOLD = {
+    "SITE_HEADER": "Fairmieten Admin",
+     "SITE_ICON": {
+        "light": lambda request: static("logo/logo_icon.svg"),  # light mode
+        "dark": lambda request: static("icon-dark.svg"),  # dark mode
+    },
+     "COLORS": {
+        "font": {
+            "subtle-light": "107 114 128",
+            "subtle-dark": "156 163 175",
+            "default-light": "75 85 99",
+            "default-dark": "209 213 219",
+            "important-light": "17 24 39",
+            "important-dark": "243 244 246",
+        },
+        "primary": {
+            "50": "250 245 255",
+            "100": "243 232 255",
+            "200": "233 213 255",
+            "300": "216 180 254",
+            "400": "192 132 252",
+            "500": "168 85 247",
+            "600": "79 158 234",
+            "700": "126 34 206",
+            "800": "107 33 168",
+            "900": "88 28 135",
+            "950": "59 7 100",
+        },
+    },
 }
