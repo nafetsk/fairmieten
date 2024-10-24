ee3521a90ad39c95ba7b2328c618ab67e51f5f22
grischan <grischan@gmail.com>
2024-10-24 13:02:27 +0200

    suche begonnen, pagination, sortierung

--- fairmieten/views.py
+++ fairmieten/views.py
@@ -1,4 +1,5 @@
 from uuid import UUID
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
@@ -18,7 +19,32 @@ def vorgang_meine_liste(request: HttpRequest) -> HttpResponse:
 
 def vorgang_liste(request: HttpRequest) -> HttpResponse:
     vorgang_liste = Vorgang.objects.all()
+    vorgang_liste = search_and_sort(request, vorgang_liste)
     return render(request, 'vorgang_liste.html', {'layout': layout(request), 'vorgang_liste': vorgang_liste})
+
+def search_and_sort(request: HttpRequest, vorgang_liste: Vorgang) -> Vorgang:
+    
+    # Suchfunktion
+    query = request.GET.get('fallnummer', None)
+    if query:
+        vorgang_liste = vorgang_liste.filter(fallnummer__icontains=query)
+
+    # Sortierung
+    sort_by = request.GET.get('sort_by', 'created')  # Standard-Sortierung
+    vorgang_liste = vorgang_liste.order_by(sort_by)
+
+    #pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(vorgang_liste, 10)  # 10 Vorgänge pro Seite
+    try:
+        vorgang_liste = paginator.page(page)
+    except PageNotAnInteger:
+        vorgang_liste = paginator.page(1)
+    except EmptyPage:
+        vorgang_liste = paginator.page(paginator.num_pages)
+
+    return vorgang_liste
+
 
 def vorgang_detail(request: HttpRequest, vorgang_id: UUID) -> HttpResponse:
     return render(request, 'vorgang_detail.html')
