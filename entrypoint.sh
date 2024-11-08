5eb2f45b41c31b1f32997b49c12cca27fe5f6d9a
grischan <grischan@gmail.com>
2024-11-08 13:14:50 +0100

    add test data again

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ PORT="${2:-8001}"
 echo "Initializing my application"
 
 poetry run python3 manage.py migrate
-#poetry run python manage.py shell -c "from aggregation.test_data import create_test_data;"
-#poetry run python manage.py shell -c "from fairmieten import insert_initial_data;"
+poetry run python manage.py shell -c "from aggregation.test_data import create_test_data;"
 
 echo "Starting my application on ${IP}:${PORT}..."
 poetry run python3 manage.py runserver ${IP}:${PORT}
