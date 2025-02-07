c411315c0c9ed3416bae2f86e5a4fc40905686aa
nafetsk <stefan_kl@posteo.de>
2025-02-07 17:00:34 +0100

    more tests

--- aggregation/tests.py
+++ aggregation/tests.py
@@ -47,5 +47,33 @@ class ChartQuerySetTest(TestCase):
             {'x_variable': 'Türkisch', 'count': 4},
             {'x_variable': 'andere', 'count': 4},
         ]
+    
+        self.assertEqual(sorted(result_qs, key=lambda x: x['x_variable']), sorted(expected_qs, key=lambda x: x['x_variable']))
         
-        self.assertEqual(list(result_qs), expected_qs)
+    def test_chart_type_2(self):
+        chart_rechtsbereich_2 = Charts.objects.get(variable="rechtsbereich")
+        
+        result_qs = get_query_set(chart_rechtsbereich_2, 2020, 2026)
+        
+        expected_qs = [
+            {'count': 13, 'x_variable': 'AGG'},
+            {'count': 6, 'x_variable': 'Sozialrecht'},
+            {'count': 12, 'x_variable': 'Mietrecht'},
+            {'count': 9, 'x_variable': 'Arbeitsrecht'},
+        ]
+        
+        self.assertEqual(sorted(result_qs, key=lambda x: x['x_variable']), sorted(expected_qs, key=lambda x: x['x_variable']))
+        
+    def test_chart_type_3(self):
+        chart_jahr_3 = Charts.objects.get(variable="datum_vorfall_von")
+        
+        result_qs = get_query_set(chart_rechtsbereich_3, 2020, 2026)
+        
+        expected_qs = [
+            {'count': 13, 'x_variable': 'AGG'},
+            {'count': 6, 'x_variable': 'Sozialrecht'},
+            {'count': 12, 'x_variable': 'Mietrecht'},
+            {'count': 9, 'x_variable': 'Arbeitsrecht'},
+        ]
+        print(expected_qs)
+        #self.assertEqual(sorted(result_qs, key=lambda x: x['x_variable']), sorted(expected_qs, key=lambda x: x['x_variable']))
