c66f0780d3fd75a5579721b45d673d666cb549a5
grischan <grischan@gmail.com>
2025-02-10 10:52:47 +0100

    no creat test_daten! vorgangsnummerstring created_by

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ PORT="${2:-8001}"
 echo "Initializing my application"
 
 poetry run python3 manage.py migrate
-poetry run python manage.py shell -c "from aggregation.test_data import create_test_data;"
+#TODO: test_data in staging seite einbauen ?
+#poetry run python manage.py shell -c "from aggregation.test_data import create_test_data;"
 
 echo "Starting my application on ${IP}:${PORT}..."
 poetry run gunicorn --bind ${IP}:${PORT} main.wsgi:application
